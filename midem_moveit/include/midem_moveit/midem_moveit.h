ce75dff604d4f77e7b118d58c4ef64f2f2c40f02
Mahisorn Wongphati <mahisorn@mantis-ubuntu>
2014-04-18 14:54:57 +0700

    remove unused header file

--- midem_moveit/include/midem_moveit/midem_moveit.h
+++ midem_moveit/include/midem_moveit/midem_moveit.h
@@ -40,7 +40,7 @@
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_model/robot_model.h>
 #include <moveit/robot_state/robot_state.h>
-#include <moveit/robot_state/joint_state_group.h>
+//#include <moveit/robot_state/joint_state_group.h>
 
 // Robot state publishing
 #include <moveit/robot_state/conversions.h>
